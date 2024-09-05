695376b944270382ee2d88433ec61f07b5c9274d
Giovanni Mansillo <giovanni.mansillo@gmail.com>
2024-09-06 00:35:15 +0200

    Frontend draft added

---
+++ site/src/Controller/DisplayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GiovanniMansillo\Component\Dory\Site\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+class DisplayController extends BaseController
+{
+    protected $default_view = 'Documents';
+    protected $app;
+}
